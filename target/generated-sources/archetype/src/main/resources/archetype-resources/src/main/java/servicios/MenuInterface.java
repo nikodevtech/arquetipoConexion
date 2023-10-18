93d8d834ecf56a55888a412dd73b3fde0c223f74
nico <nikoalvarezzapata@gmail.com>
2023-10-18 12:43:26 +0200

    connection bbdd archetype

---
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/servicios/MenuInterface.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package servicios;
+
+/**
+ * Interface que contiene los metodos que dan servicio al menu de opciones
+ */
+public interface MenuInterface {
+
+	/**
+	 * Método encargado de mostrar al usuario un menú por consola con las opciónes
+	 * disponibles
+	 * 
+	 * @return Número entero en relación a la opción seleccionada (-1 en caso de ser null el valor)
+	 */
+	public int mostrarMenu();
+}
---
+++ target/generated-sources/archetype/target/classes/archetype-resources/src/main/java/servicios/MenuImpl.java
@@ -0,0 +1,31 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package servicios;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Clase que implementa la interfaz MenuInterface y es donde se detalla la funcionalidad
+ * concreta de los métodos de dicha interfaz.
+ */
+public class MenuImpl implements MenuInterface{
+	
+	@Override
+	public int mostrarMenu() {
+		try {
+		return Integer.parseInt(JOptionPane.showInputDialog(
+				"${symbol_escape}n--- MENU ---${symbol_escape}n" + "${symbol_escape}n1. Registrar libro${symbol_escape}n" + "2. Mostrar libro${symbol_escape}n3. Modificar libro"
+						+ "${symbol_escape}n4. Eliminar libro${symbol_escape}n0. Salir${symbol_escape}n" + "${symbol_escape}nIntroduce la opción deseada: "));
+		}catch(NumberFormatException nfe) {
+			System.out.println("**ERROR MenuImpl MostrarMenu** Error con el tipo de dato al convertir a string: " + nfe);
+			return -1;
+		}catch(HeadlessException he) {
+			System.out.println("**ERROR MenuImpl MostrarMenu** Error el usuario no tiene interfaz gráfica: " + he);
+			return -1;
+		}
+	}
+
+}
